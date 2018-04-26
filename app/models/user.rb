8bc6d1f800496f29356f94b0ed1d7f714336d04b
yoshihiko sughimura <yoikosugi@gmail.com>
2018-04-26 23:51:21 +0900

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
-end
+    has_many :microposts
+    validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+    validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
+  end
